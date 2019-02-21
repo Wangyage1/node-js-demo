ae35f7792190f354353cae86b507b87f3eb3ce05
王亚鸽 <wangyage@cvte.com>
2019-02-21 11:00:53 +0800

    [modify][what] socket 网络编程

--- networking/net-watcher-json-client.js
+++ networking/net-watcher-json-client.js
@@ -4,18 +4,33 @@ const client = require('net').connect({
     port: 60300
 });
 
-const ldjClient = require('./lib/ldj-client.js').connect(client);
-
-ldjClient.on('message', message => {
-    
-    console.log('接收到消息吗');
-    if (message.type === 'watching') {
-        console.log(`Now watching: ${message.file}`);
-    } else if (message.type === 'changed') {
-        const date = new Date(message.timestamp);
-
-        console.log(`File changed: ${date}`);
-    } else {
-        console.log(`Unrecongized message type: ${message.type}`);
+var buffer = '';
+client.on('data', data => {
+    buffer+= data;
+    let boundary = buffer.indexOf('\n');
+    console.log('buffer', buffer.toString());
+    while(boundary !== -1) {
+        const input = buffer.substring(0, boundary);
+        buffer = buffer.substring(boundary + 1);
+        console.log('message', JSON.parse(input));
+        boundary = buffer.indexOf('\n');
     }
 })
+
+
+// const ldjClient = require('./lib/ldj-client.js').connect(client);
+
+// ldjClient.on('message', message => {
+    
+//     console.log('接收到消息吗');
+//     if (message.type === 'watching') {
+//         console.log(`Now watching: ${message.file}`);
+//     } else if (message.type === 'changed') {
+//         const date = new Date(message.timestamp);
+
+//         console.log(`File changed: ${date}`);
+//     } else {
+//         console.log(`Unrecongized message type: ${message.type}`);
+//     }
+// })
+
--- networking/server.js
+++ networking/server.js
@@ -1,6 +1,29 @@
 
-const net = require('net');
-const server = net.createServer((connect) => {
 
+const fs = require('fs');
+const net = require('net');
+const filename = process.argv[2];
+
+if (!filename) {
+    console.log('文件不存在！');
+    return;
+}
+
+const server = net.createServer((connection) => {
+    console.log('已建立连接');
+    connection.write(JSON.stringify({type: 'watching', file: filename} + '\n'));
+
+    const watcher = fs.watch(filename, () => {
+        connection.write(JSON.stringify({type: 'changed', timestamp: Date.now()}) + '\n');
+    })
+
+
+    connection.on('close', () => {
+        console.log('连接关闭');
+        watcher.close();
+    })
 });
-server.listen(6000);
+
+server.listen(60300, () => {
+    console.log('server is listening 60300');
+})
